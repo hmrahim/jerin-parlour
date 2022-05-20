2399afb803f67bdfd34fb3ef0581682f10a67728
hmrahim <h.m.rahimnet@gmail.com>
2022-05-21 02:01:10 +0600

    after relode redirect to login page problem is done

--- src/Components/Auth/RequireAuth.js
+++ src/Components/Auth/RequireAuth.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { Navigate, Outlet, useLocation } from 'react-router-dom';
-import auth from '../../firebase.init';
+import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import auth from "../../firebase.init";
 
 const RequireAuth = () => {
-    const [user,loading,error] = useAuthState(auth)
-    const location = useLocation()
-    if(!user){
-        return <Navigate to="/login" state={{from:location}} />
-    }
-    return <Outlet/>
+  const [user, loading, error] = useAuthState(auth);
+  const location = useLocation();
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center mt-10">
+        <button class="btn loading">loading</button>
+      </div>
+    );
+  }
 
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} />;
+  }
+
+  return <Outlet />;
 };
 
 export default RequireAuth;
